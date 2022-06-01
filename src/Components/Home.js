60c7a142860cebc384b2d058f3930518c9c02cb9
Richard <cguo3889@uni.sydney.edu.au>
2022-06-01 16:56:54 +1000

    finish

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,9 +13,9 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-    
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import styles from "./mystyle.module.css";
 
 //rgb(114, 18, 240) background color;
 
@@ -24,7 +24,7 @@ const useStyles = makeStyles({
     width: "100%",
     height: "100%",
     backgroundColor: "rgb(114, 18, 240)",
-    position: "fixed",
+    position: "relative",
     left: "0",
   },
   title: {
@@ -36,6 +36,7 @@ const useStyles = makeStyles({
     color: "white",
     fontWeight: "bold",
     fontSize: "25px",
+    boxShadow: "5px 5px 15px 5px #000000",
   },
   titleText: {
     marginTop: "0px",
@@ -56,11 +57,76 @@ const useStyles = makeStyles({
     border: "3px solid",
     textTransform: "capitalize",
   },
+
+  dialogStyle: {
+    width: "100%",
+    height: "800px",
+  },
+
+  postInputStyle: {
+    color: "blue",
+    borderStyle: "solid",
+    borderColor: "rgb(114, 18, 240)",
+  },
+  notchedOutline: {
+    borderWidth: "2px",
+    borderStyle: "solid",
+    borderColor: "rgb(114, 18, 240) !important",
+    height: "auto",
+    // marginBottom: "400px",
+  },
+
+  formStyle: {
+    display: "flex",
+    flexDirection: "column",
+    width: "600px",
+    height: "400px",
+    // borderStyle: "solid",
+  },
+  textareaStyle: {
+    paddingLeft: "30px",
+    paddingTop: "30px",
+    fontStyle: "normal",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    fontSize: "20px",
+    borderRadius: "30px",
+    borderColor: "rgb(114, 18, 240)",
+    borderStyle: "solid",
+    borderWidth: "3px",
+    boxShadow: "3px 4px 10px -2px #000000",
+  },
+
+  inputStyle: {
+    marginTop: "30px",
+    height: "50px",
+    paddingLeft: "30px",
+    fontStyle: "normal",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    fontSize: "20px",
+    borderRadius: "25px",
+    borderColor: "rgb(114, 18, 240)",
+    borderStyle: "solid",
+    borderWidth: "3px",
+    boxShadow: "3px 4px 10px -2px #000000",
+  },
+  postButton: {
+    marginTop: "40px",
+    width: "40%",
+    height: "60px",
+    backgroundColor: "rgb(114, 18, 240)",
+    borderColor: "rgb(114, 18, 240)",
+    color: "white",
+    fontSize: "23px",
+    fontWeight: "bold",
+    cursor: "pointer",
+    borderRadius: "30px",
+    borderStyle: "solid",
+    borderWidth: "3px",
+  },
 });
 function Home(props) {
   const [posts, setPosts] = useState([]);
   const [selectPost, setSelectPost] = useState({});
-
 
   const [post, setPost] = useState("");
   const [pseudonym, setPseudonym] = useState("");
@@ -76,7 +142,6 @@ function Home(props) {
     setOpen(false);
   };
 
-
   // function handleReplyOpen(id) {
   //     setReply(true);
   // }
@@ -87,11 +152,17 @@ function Home(props) {
 
   const showInfo = (id) => {
     console.log(id);
-    setSelectPost(posts.find(it => it.id === id))
-    setReply(true)
-  
-  }
- 
+    setSelectPost(posts.find((it) => it.id === id));
+    setReply(true);
+  };
+
+  const customStyles = {
+    width: "100%",
+    height: "500px",
+    justifyContent: "center",
+    textalign: "center",
+    alignItems: "center",
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -119,27 +190,28 @@ function Home(props) {
   };
   const handleReplyOpen = () => {
     //  console.log(e);
-      //console.log(index);
-    
+    //console.log(index);
 
-     setReply(true);
-    };
+    setReply(true);
+  };
   const voteUp = (index) => {
     const newPosts = [...posts];
 
     newPosts[index].voteNo++;
 
     setPosts(newPosts);
-   // calculateTotal();
-};
+    // calculateTotal();
+  };
 
-const voteDown = (index) => {
+  const voteDown = (index) => {
     const newPosts = [...posts];
 
     newPosts[index].voteNo--;
 
     setPosts(newPosts);
-};
+  };
+
+  const styles = (theme) => ({});
 
   return (
     <>
@@ -172,86 +244,186 @@ const voteDown = (index) => {
           </Grid>
         </Grid>
 
-        <Dialog open={open} onClose={handleClose}>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          //   style={customStyles}
+          sx={{
+            "& .MuiDialog-container": {
+              "& .MuiPaper-root": {
+                // width: "150%",
+                minWidth: "60%",
+                marginTop: "200px",
+                height: "500px",
+                justifyContent: "center",
+                textalign: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        >
           <form onSubmit={handleSubmit}>
             <DialogContent>
-              <TextField
+              {/* <TextField
                 autoFocus
                 margin="dense"
                 id="post"
-                label="post"
+                label="Write your post ..."
                 type="text"
                 value={post}
                 fullWidth
-                variant="standard"
                 onChange={handlePost}
-                required
-              />
-
-              <TextField
+                variant="outlined"
+              rows="10"
+                InputProps={{
+                  classes: {
+                    notchedOutline: classes.notchedOutline,
+                  },
+                }}
+              /> */}
+              <div width="100%" className={classes.formStyle}>
+                <textarea
+                  className={classes.textareaStyle}
+                  rows="6"
+                  columns="10"
+                  id="post"
+                  type="text"
+                  placeholder="Write your post ..."
+                  required
+                  onChange={handlePost}
+                />
+                <input
+                  className={classes.inputStyle}
+                  id="pseudonym"
+                  type="text"
+                  placeholder="Enter your pseudonym"
+                  required
+                  onChange={handlePseudonym}
+                />
+                <button type="submit" className={classes.postButton}>
+                  Post
+                </button>
+              </div>
+              {/* <TextField
                 autoFocus
                 margin="dense"
                 id="pseudonym"
-                label="pseudonym"
+                label="Enter your pseudonym"
                 type="text"
                 fullWidth
                 value={pseudonym}
-                variant="standard"
+                variant="outlined"
                 onChange={handlePseudonym}
-                required
-              />
+                InputProps={{
+                  classes: {
+                    notchedOutline: classes.notchedOutline,
+                  },
+                }}
+              /> */}
             </DialogContent>
-            <DialogActions>
-              <Button type="submit">Post</Button>
-            </DialogActions>
           </form>
         </Dialog>
 
-        <Typography component={"div"} backgroundColor="white">
+        <div className="container">
           {posts.map((item, index) => (
-            <div key={index}>
-              <p>
-                {item.post} {item.pseudonym} {item.voteNo}
-              </p>
-              <KeyboardArrowUpIcon cursor="pointer" onClick={() => voteUp(index)}/>
-              <KeyboardArrowDownIcon cursor="pointer" onClick={() => voteDown(index)} />
-              <button className={classes.buttonStyle} onClick={() => showInfo(item.id)}>
-                Reply
-              </button>
-             
-              <Dialog open={reply} onClose={handleReplyClose}>
-                
-                
-                  <DialogContent>
-                    <TextField
-                      autoFocus
-                      margin="dense"
-                      id="post"
-                      label="post"
-                      type="text"
-                      fullWidth
-                      variant="standard"
-                    />
+            <div key={index} className="postItem">
+              <div className="voteContainer">
+                <KeyboardArrowUpIcon
+                  className="voteIcon"
+                  cursor="pointer"
+                  fontSize="2.5rem"
+                  onClick={() => voteUp(index)}
+                />
+                <p className="voteNo"> {item.voteNo}</p>
+                <KeyboardArrowDownIcon
+                  className="voteIcon"
+                  cursor="pointer"
+                  fontSize="2.5rem"
+                  onClick={() => voteDown(index)}
+                />
+              </div>
+              <div className="itemDetail">
+                <p className="postDetail">{item.post}</p>
+                <p className="postPseudoym">{item.pseudonym}</p>
+                <button
+                  className="replyButton"
+                  onClick={() => showInfo(item.id)}
+                >
+                  Reply
+                </button>
+              </div>
 
-                    <TextField
-                      autoFocus
-                      margin="dense"
+              <Dialog
+                open={reply}
+                onClose={handleReplyClose}
+                sx={{
+                  "& .MuiDialog-container": {
+                    "& .MuiPaper-root": {
+                      // width: "150%",
+                      minWidth: "60%",
+                      marginTop: "200px",
+                      height: "500px",
+                      justifyContent: "center",
+                      textalign: "center",
+                      alignItems: "center",
+                    },
+                  },
+                }}
+              >
+                <DialogContent>
+                  <div width="100%" className={classes.formStyle}>
+                    <textarea
+                      className={classes.textareaStyle}
+                      rows="6"
+                      columns="10"
+                      id="post"
+                      type="text"
+                      placeholder="Write your post ..."
+                    />
+                    <input
+                      className={classes.inputStyle}
                       id="pseudonym"
-                      label="pseudonym"
                       type="text"
                       text={selectPost.pseudonym}
                       value={selectPost.pseudonym}
-                      fullWidth
-                      variant="standard"
                     />
-                  </DialogContent>
-                  <DialogActions>
-                    <Button onClick={handleReplyClose}>Post</Button>
-                  </DialogActions>
+                    <button
+                      type="submit"
+                      onClick={handleReplyClose}
+                      className={classes.postButton}
+                    >
+                      Post
+                    </button>
+                  </div>
+                  {/* <TextField
+                    autoFocus
+                    margin="dense"
+                    id="post"
+                    label="post"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                  />
+
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="pseudonym"
+                    label="pseudonym"
+                    type="text"
+                    text={selectPost.pseudonym}
+                    value={selectPost.pseudonym}
+                    fullWidth
+                    variant="standard"
+                  /> */}
+                </DialogContent>
+                {/* <DialogActions>
+                  <Button onClick={handleReplyClose}>Post</Button>
+                </DialogActions> */}
               </Dialog>
             </div>
           ))}
-        </Typography>
+        </div>
       </div>
     </>
   );
