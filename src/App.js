dfc2d7b8d64b839e6e90e1d827db609fb93e0773
Richard <cguo3889@uni.sydney.edu.au>
2022-06-01 18:55:59 +1000

    rm app.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import { StyledEngineProvider } from "@mui/material/styles";
